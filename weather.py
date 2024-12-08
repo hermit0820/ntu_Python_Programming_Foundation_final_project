7a91c228eeabd6498b80d0a15f11eedb2be2ef0a
yuma0418c <20040418george@gmail.com>
2024-12-08 19:41:04 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -181,9 +181,9 @@ city_label.pack(anchor="w", padx=20)
 city_var = tk.StringVar()
 city_list = [
     "請選擇欲查詢城市","臺北市", "新北市", "基隆市", "桃園市", "新竹市", "新竹縣",
-    "苗栗縣", "台中市", "彰化縣", "南投縣", "雲林縣", "嘉義市",
-    "嘉義縣", "台南市", "高雄市", "屏東縣", "宜蘭縣",
-    "花蓮縣", "台東縣", "澎湖縣", "金門縣", "連江縣"
+    "苗栗縣", "臺中市", "彰化縣", "南投縣", "雲林縣", "嘉義市",
+    "嘉義縣", "臺南市", "高雄市", "屏東縣", "宜蘭縣",
+    "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"
 ]
 city_dropdown = ttk.Combobox(root, textvariable=city_var, values=city_list)
 city_dropdown.pack(padx=20, pady=5)
